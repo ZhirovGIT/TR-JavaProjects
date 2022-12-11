2b3f5eb65512844d661b46cd347868e76d705547
Dmitrii <111457490+ZhirovGIT@users.noreply.github.com>
2022-12-11 20:34:34 +0200

    Update Book.java

--- Webinar_20/src/Book.java
+++ Webinar_20/src/Book.java
@@ -17,31 +17,39 @@ public class Book {
 			title = title01;
 		else
 			title = "No name";
-		
+
 		if (author01 != null)
 			author = author01;
 		else
 			author = "Anonymous";
-		
+
 		if (yearOfIssue01 > 1700 && yearOfIssue01 <= 2022)
 			yearOfIssue = yearOfIssue01;
-		
+
 		if (genre01 != null)
 			genre = genre01;
 		else
 			genre = "No genre";
-		
-		cover = cover01;	// boolean
-		
+
+		cover = cover01; // boolean
+
 		// My-00000
 		char[] id = identifier01.toCharArray();
-		if (id.length == 8 && id[0] == 'M' && id[1] == 'y' &&
-				id[2] == '-')
-		{
-			for(int i = 3; i < id.length; i++)
-			{
-				if (id[i] >= '0')
+		boolean flag = false;
+		if (id.length == 8 && id[0] == 'M' && id[1] == 'y' && id[2] == '-') {
+			for (int i = 3; i < id.length; i++) {
+				if (id[i] >= '0' && id[i] <= '9')
+					flag = true;
+				else {
+					flag = false;
+					break;
+				}
 			}
+
+			if (flag) // (flag) -> true; (!flag) -> false
+				identifier = identifier01;
+			else
+				System.out.println("Wrong identifier!");
 		}
 	}
 
